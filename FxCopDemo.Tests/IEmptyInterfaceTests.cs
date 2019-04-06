3e9d0bc7d1ddc0b92c3d7bdd050ce2905d33f07d
Adriano Ueda <adriano.ueda@gmail.com>
2019-04-06 11:15:55 -0400

    Add another example to the FxCop demo.

---
+++ FxCopDemo.Tests/IEmptyInterfaceTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Adriano Ueda. All rights reserved.
+
+namespace FxCopDemo.Tests
+{
+    using FxCopDemo;
+    using Xunit;
+
+    public class IEmptyInterfaceTests
+    {
+        /// <summary>
+        /// Returns the correct name for a given number
+        /// if exists.
+        /// </summary>
+        [Fact]
+        public void GetNameReturnsTheNameIfExits()
+        {
+            var mockObject = new MockEmptyInterface();
+
+            var actual = mockObject is IEmptyInterface;
+
+            Assert.True(actual);
+        }
+
+        /// <summary>
+        /// Implements a mock class.
+        /// </summary>
+        private class MockEmptyInterface
+            : IEmptyInterface
+        {
+            /// <summary>
+            /// Returns the name of the mock object.
+            /// </summary>
+            /// <returns>The name of mock object.</returns>
+            public string GetName()
+            {
+                return "Mockup Empty Interface";
+            }
+        }
+    }
+}
---
+++ FxCopDemo/IEmptyInterface.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Adriano Ueda. All rights reserved.
+
+namespace FxCopDemo
+{
+    /// <summary>
+    /// Empty interface for demo purposes.
+    /// </summary>
+    public interface IEmptyInterface
+    {
+#if !DISABLE_FIXES
+        /// <summary>
+        /// Gets the property name.
+        /// </summary>
+        /// <returns>The name.</returns>
+        string GetName();
+#endif
+    }
+}

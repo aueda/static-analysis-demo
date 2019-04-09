7a6c27f89ecf4e0c1e0059880aa6a4352f603024
Adriano Ueda <adriano.ueda@gmail.com>
2019-04-08 20:23:27 -0400

    Add suppress message example to the project.

---
+++ FxCopDemo.Tests/IEmptyInterface2Tests.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Adriano Ueda. All rights reserved.
+
+namespace FxCopDemo.Tests
+{
+    using FxCopDemo;
+    using Xunit;
+
+    public class IEmptyInterface2Tests
+    {
+        /// <summary>
+        /// Tests if mock class implements interface.
+        /// </summary>
+        [Fact]
+        public void TestsIfObjectImplementsInterface()
+        {
+            var mockObject = new MockEmptyInterface();
+
+            var actual = mockObject is IEmptyInterface2;
+
+            Assert.True(actual);
+        }
+
+        /// <summary>
+        /// Implements a mock class.
+        /// </summary>
+        private class MockEmptyInterface
+            : IEmptyInterface2
+        {
+        }
+    }
+}
---
+++ FxCopDemo/IEmptyInterface2.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Adriano Ueda. All rights reserved.
+
+namespace FxCopDemo
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    /// <summary>
+    /// Empty interface for demo purposes.
+    /// </summary>
+#if !DISABLE_FIXES
+    [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces")]
+#endif
+    public interface IEmptyInterface2
+    {
+    }
+}
